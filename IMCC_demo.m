6f09080f561ae5de5bd485813341b91614ce67df
keauneuh <51431491+keauneuh@users.noreply.github.com>
2019-06-06 13:15:06 +0800

    Update IMCC_demo.m

--- IMCC_demo.m
+++ IMCC_demo.m
@@ -5,9 +5,9 @@ addpath('IMCCdata/');
 
 load('Yeast');
 
-param.numCluster = 8;  % numCluster ranges from 2.^{3, 4, 5, 6, 7, 8}
+param.numCluster = 8; % numCluster ranges from 2.^{3, 4, 5, 6, 7, 8}
 param.alpha = 1; % alpha ranges from 10^{-3, -2, -1, 0, 1, 2, 3}
 param.beta = 0.1; % beta ranges from 10^{-3, -2, -1, 0, 1, 2, 3}
 param.gamma = 1; % gamma ranges from 10^{-3, -2, -1, 0, 1, 2, 3}
 
-[results] = IMCC( train_data, train_target, test_data, test_target, param);
+[results] = IMCC( train_data, train_target, test_data, test_target, param );
